d443ad347c23791682264cacd633d39d305b7e8f
ItayHadas <5610394+ItayHadas@users.noreply.github.com>
2025-08-09 00:39:17 +0000

    Deploying to gh-pages from @ ItayHadas/ItayHadas.github.io@5abe8d70f9157d247eab326fa88e2b6cd1ec0d22 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,26 +32,19 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{
+        },{id: "nav-submenus",
+          title: "submenus",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/_pages/dropdown/";
+          },
+        },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
